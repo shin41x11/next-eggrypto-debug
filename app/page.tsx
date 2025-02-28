913bb9b9b50e4051076b9422b3a586f5f686344a
yokoi-shintaro <32443626+yokoi-shintaro@users.noreply.github.com>
2025-02-28 15:19:00 +0900

    homeにもスタイルシート適用

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,12 @@
 "use client";
 import React from 'react';
+import { styles } from './styles/dashboardStyles';
 
 export default function Home() {
   return (
-    <main style={{ padding: "2rem" }}>
-      <h1>Home</h1>
-      <p></p>
+    <main style={styles.main}>
+      <h1 style={styles.header}>Home</h1>
+      <p>Welcome to this Page.This is studying</p>
     </main>
   );
 }
