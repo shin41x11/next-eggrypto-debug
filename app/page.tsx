625b49a0687968805867084445c10b00f51183ec
yokoi-shintaro <32443626+yokoi-shintaro@users.noreply.github.com>
2025-02-28 16:30:24 +0900

    文言調整中

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,12 @@ export default function Home() {
   return (
     <main style={styles.main}>
       <h1 style={styles.header}>Home</h1>
-      <p>Welcome to this Page.This is studying</p>
+      <p>Welcome to this Page. This is studying page for <a href="https://university.alchemy.com/" style={styles.link}>Alchemy University</a>.</p>
+
+      <section style={styles.section}>
+        <h2 style={styles.sectionTitle}>Words</h2>
+        <p>The quick brown fox jumps over the lazy dog.</p>
+      </section>
     </main>
   );
 }
